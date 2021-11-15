fc3637e14ced90131ec76b4ba6deb302b2525ecb
kennyincisowow <kennyinciso@upeu.edu.pe>
2021-11-15 13:38:22 -0500

    one commit
    hey locos

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
-const contenedor = document.querySelector('#contenedor');
+$(document).ready(function () {
+	// Inicializar información sobre herramientas
+	$('[data-toggle="tooltip"]').tooltip();
 
-document.querySelector('#boton-menu').addEventListener('click', () => {
-	contenedor.classList.toggle('active');
-});
+	// Agregue un desplazamiento suave a todos los enlaces en la barra de navegación + enlace de pie de página
+	$(".navbar a, footer a[href='#myPage']").on('click', function (event) {
 
-const comprobarAncho = () => {
-	if(window.innerWidth <= 768){
-		contenedor.classList.remove('active');
-	} else {
-		contenedor.classList.add('active');
-	}
-}
+		// Asegúrese de que this.hash tenga un valor antes de anular el comportamiento predeterminado
+		if (this.hash !== "") {
 
-comprobarAncho();
+			// Evita el comportamiento predeterminado del clic de anclaje
+			event.preventDefault();
 
-window.addEventListener('resize', () => {
-	comprobarAncho();
-});
+			// Almacenar hash
+			var hash = this.hash;
+
+			// Usando el método jQuery animate () para agregar un desplazamiento suave de la página 
+			// El número opcional (900) especifica la cantidad de milisegundos que se necesitan para desplazarse al área especificada
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 600, function () {
+
+				// Agregue hash (#) a la URL cuando termine de desplazarse (comportamiento de clic predeterminado)
+				window.location.hash = hash;
+			});
+		} // Terminara 
+	});
+})
